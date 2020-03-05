ae8d0c1d3940a92a0447108b105c60f33ed8f0f3
Gennady Yonov <g.jonovs@mail.ru>
2020-03-05 17:10:04 +0200

    Hello okta spa okta react v2 (#1)
    * -- migration to okta-react v2.0.0
    
    * get access token from AuthService

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {AppRouterConnected} from './AppRouterConnected';
 import {store} from './store';
+import {authService} from './auth';
+import {HomeConnected} from '../components/Home/HomeConnected';
+import {LoginCallback, SecureRoute, Security} from '@okta/okta-react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 export const App: React.FC = () => (
   <Provider store={store}>
-    <AppRouterConnected/>
+    <Router>
+      <Security authService={authService}>
+        <Switch>
+          <SecureRoute exact path="/" component={HomeConnected}/>
+          <Route path='/implicit/callback' component={LoginCallback}/>
+        </Switch>
+      </Security>
+    </Router>
   </Provider>
 );
