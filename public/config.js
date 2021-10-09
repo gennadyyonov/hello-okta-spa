24a468bd5559b3bbeff4a78443213ec8d94d80a2
Gennady Yonov <g.jonovs@mail.ru>
2021-10-09 20:43:17 +0300

    -- Get CSRF info from BE

--- public/config.js
+++ public/config.js
@@ -1,4 +1,5 @@
 window.REACT_APP_NODE_ENV = 'production';
 window.REACT_APP_BFF = '';
 window.REACT_APP_ENVIRONMENT_CONFIG = '';
+window.REACT_APP_CSRF_TOKEN_INFO = '';
 window.REACT_APP_LOGOUT = '';
--- src/helpers/config.tsx
+++ src/helpers/config.tsx
@@ -4,5 +4,6 @@ export class Config {
     static nodeEnv = process.env.REACT_APP_NODE_ENV || envSettings.REACT_APP_NODE_ENV;
     static bffUrl = process.env.REACT_APP_BFF || envSettings.REACT_APP_BFF;
     static environmentConfigUrl = process.env.REACT_APP_ENVIRONMENT_CONFIG || envSettings.REACT_APP_ENVIRONMENT_CONFIG;
+    static csrfTokenInfoUrl = process.env.REACT_APP_CSRF_TOKEN_INFO || envSettings.REACT_APP_CSRF_TOKEN_INFO;
     static logoutUrl = process.env.REACT_APP_LOGOUT || envSettings.REACT_APP_LOGOUT;
 }
