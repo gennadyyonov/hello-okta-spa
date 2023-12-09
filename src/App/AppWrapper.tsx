0a433917c9d4737d9321f101eb8938de444f2ef6
Gennady Yonov <g.jonovs@mail.ru>
2023-12-09 20:55:42 +0200

    -- uninstall @mui/styles as it has dependency to react 17

--- src/App/AppWrapper.tsx
+++ src/App/AppWrapper.tsx
@@ -1,21 +1,19 @@
-import { WithStyles, withStyles } from '@mui/styles';
 import Paper from '@mui/material/Paper';
 import React, { ReactNode } from 'react';
 
-const styles = {
-  root: {
-    margin: 20,
-    padding: 20,
-    width: 400,
-    height: 400
-  }
-};
-
 // @ts-ignore
-const AppWrapper: React.FC<{ children?: ReactNode } & WithStyles> = ({ children, classes }) => (
-  <Paper className={classes.root} elevation={3}>
+const AppWrapper: React.FC<{ children?: ReactNode }> = ({ children, classes }) => (
+  <Paper
+    sx={{
+      margin: 20,
+      padding: 20,
+      width: 400,
+      height: 400
+    }}
+    elevation={3}
+  >
     {children}
   </Paper>
 );
 
-export default withStyles(styles)(AppWrapper)
+export default AppWrapper
