b014e8219f0c28af240a06ef82d447b65d498813
Gennady Yonov <gennady.yonov@gmail.com>
2023-12-10 07:14:15 +0200

    Migrate from react router v5 to v6 (#47)

---
+++ src/App/RequiredAuth.tsx
@@ -0,0 +1,25 @@
+import { useOktaAuth } from '@okta/okta-react';
+import React, { useEffect } from 'react';
+import { Outlet } from 'react-router-dom';
+import { environmentConfig } from '../helpers/environmentConfig';
+
+export const RequiredAuth: React.FC = () => {
+  const { authState } = useOktaAuth();
+  const { oktaAuth } = environmentConfig;
+
+  useEffect(() => {
+    if (!oktaAuth || !authState) {
+      return;
+    }
+
+    if (!authState?.isAuthenticated) {
+      oktaAuth.signInWithRedirect();
+    }
+  }, [oktaAuth, authState]);
+
+  if (!authState || !authState?.isAuthenticated) {
+    return null;
+  }
+
+  return (<Outlet />);
+};
