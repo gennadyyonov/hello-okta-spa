217612c804aa86d66d1f41ae090062427c39d126
Gennady Yonov <g.jonovs@mail.ru>
2023-12-10 06:11:49 +0200

    -- Test setup

---
+++ src/helpers/parseTranslations.test.tsx
@@ -0,0 +1,24 @@
+import { TranslationsMapEntries } from './initTranslations';
+import { parseTranslations } from './parseTranslations';
+
+describe('parseTranslations', () => {
+  it('should return translations', () => {
+    const translationEntries: TranslationsMapEntries[] = [
+      {
+        key: 'home_button_ping',
+        value: 'Ping',
+      },
+      {
+        key: 'button_logout',
+        value: 'Logout',
+      },
+    ];
+
+    const actual = parseTranslations(translationEntries);
+
+    expect(actual).toStrictEqual({
+      'home_button_ping': 'Ping',
+      'button_logout': 'Logout',
+    });
+  });
+});
