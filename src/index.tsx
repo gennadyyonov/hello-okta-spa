ee71c9382b241b63d91650f448dcc246589aaa6f
Gennady Yonov <g.jonovs@mail.ru>
2025-03-29 07:10:09 +0200

    Dependency clean-up

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
 import '@okta/okta-auth-js/polyfill';
 // https://giters.com/facebook/react/issues/20598
 import './App/renderApp';
