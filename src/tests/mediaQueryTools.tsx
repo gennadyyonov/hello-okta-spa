ccb098e64cc492fa204896a6e37575d7f71e0cae
Gennady Yonov <gennady.yonov@gmail.com>
2026-07-04 22:50:36 +0300

    Update dependency prettier to v3.9.4

--- src/tests/mediaQueryTools.tsx
+++ src/tests/mediaQueryTools.tsx
@@ -1,19 +1,16 @@
 import mediaQuery from 'css-mediaquery';
 
 export const defineWindowMatchMedia = (width: number = 0) => {
-  vi.stubGlobal(
-    'matchMedia',
-    (query: string): MediaQueryList => ({
-      media: query,
-      matches: query === '(pointer: fine)' || (width > 0 && mediaQuery.match(query, { width })),
-      onchange: null,
-      addEventListener: vi.fn(),
-      removeEventListener: vi.fn(),
-      addListener: vi.fn(),
-      removeListener: vi.fn(),
-      dispatchEvent: vi.fn(() => false),
-    }),
-  );
+  vi.stubGlobal('matchMedia', (query: string): MediaQueryList => ({
+    media: query,
+    matches: query === '(pointer: fine)' || (width > 0 && mediaQuery.match(query, { width })),
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    dispatchEvent: vi.fn(() => false),
+  }));
 };
 
 export const defineDesktopSizeWindowMatchMedia = () => {
